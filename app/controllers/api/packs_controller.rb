509ef5c488e020c05173d850af9a031ef1ec4ce3
ryan <ryanwaszak@gmail.com>
2020-03-07 21:16:51 -0600

    add user friendly views

--- app/controllers/api/packs_controller.rb
+++ app/controllers/api/packs_controller.rb
@@ -1,21 +1,35 @@
 class Api::PacksController < ApplicationController
   def index
+    @packs = Pack.all
     render "index.json.jb"
   end
 
   def create
-    render "create.json.jb"
+    @pack = Pack.new(
+      name: params[:name],
+      price: params[:price],
+      color: params[:color]
+    )
+    @pack.save
+    render "show.json.jb"
   end
 
   def show
+    @pack = Pack.find_by(id: params[:id])
     render "show.json.jb"
   end
 
   def update
-    render "update.json.jb"
+    @pack = Pack.find_by(id: params[:id])
+    @pack.name = params[:name] || @pack.name
+    @pack.color = params[:color] || @pack.color
+    @pack.price = params[:price] || @pack.price
+    render "show.json.jb"
   end
 
   def destroy
-    render "destroy.json.jb"
+    pack = Pack.find_by(id: params[:id])
+    pack.destroy
+    render json: {message: "Pack successfully destroyed."}
   end
 end
